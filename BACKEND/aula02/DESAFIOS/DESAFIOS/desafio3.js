0dcf5641b1a87286f7cc262bf42f71ce4e0e4602
Rebecca Ferreira <becca.hernandes@hotmail.com>
2026-08-21 14:43:50 -0300

    21/08

--- BACKEND/aula02/DESAFIOS/DESAFIOS/desafio3.js
+++ BACKEND/aula02/DESAFIOS/DESAFIOS/desafio3.js
@@ -4,13 +4,16 @@ function calcularArea (n1, n2){
     return n1 * n2 
 }
 
-const largura = entrada.questionInt("Qual é a largura do terreno? \n");
-const comprimento = entrada.questionInt("Qual é o comprimento do terreno? \n ")
+const largura = entrada.questionInt("Qual e a largura do primeiro terreno? \n");
+const comprimento = entrada.questionInt("Qual e o comprimento do primeiro terreno? \n ")
 
-const largura2 = entrada.questionInt("Qual é a largura do terreno? \n");
-const comprimento2 = entrada.questionInt("Qual é o comprimento do terreno? \n ")
+const largura2 = entrada.questionInt("Qual e a largura do segundo terreno? \n");
+const comprimento2 = entrada.questionInt("Qual e o comprimento do segundo terreno? \n ")
 
-const largura3 = entrada.questionInt("Qual é a largura do terreno? \n");
-const comprimento3 = entrada.questionInt("Qual é o comprimento do terreno? \n ")
+const largura3 = entrada.questionInt("Qual e a largura do terceiro terreno? \n");
+const comprimento3 = entrada.questionInt("Qual e o comprimento do terceiro terreno? \n ")
 
 const area = largura * comprimento
+console.log(`A primeira area é ${calcularArea(largura,comprimento)}`);
+console.log(`A segunda area é ${calcularArea(largura2,comprimento2)}`);
+console.log(`A terceira area é ${calcularArea(largura3,comprimento3)}`);
