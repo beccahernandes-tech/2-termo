469d6600c34e7537e7f119c2d2dccd53833a2d72
Rebecca Ferreira <becca.hernandes@hotmail.com>
2026-09-10 11:25:05 -0300

    10/09

--- BCD/aula 06/script_cr.sql
+++ BCD/aula 06/script_cr.sql
@@ -28,3 +28,30 @@ INSERT INTO CLIENTE (NOME_CLIENTE) VALUES
 ('BRUNO'),
 ('REBECCA'),
 ('MARIA');
+
+-- 1
+-- Uma categoria pode possuir vários produtos. Cada produto pertence a apenas uma categoria.
+
+-- 1,N E 1,1
+
+-- 2
+-- Um funcionário pode registrar vários pedidos. Cada pedido é registrado por um funcionário.
+
+-- 1,N E 1,1
+
+-- 3
+-- Um fornecedor comercializa vários produtos, e o mesmo produto pode ser comprado de vários fornecedores.
+
+-- 1,N E 1,N
+
+
+-- 4
+-- Uma mesa pode existir sem nenhuma reserva futura. Uma reserva deve estar vinculada a uma mesa.
+
+-- 0,N E 1,1
+
+
+--5
+-- Um pedido possui vários itens. Um item de pedido pertence a um único pedido.
+
+-- 1,N E 1,1
