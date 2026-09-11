134ad28891a1fd64825c05755f97540cedbfc25d
Rebecca Ferreira <becca.hernandes@hotmail.com>
2026-09-11 11:23:48 -0300

    novos

--- PBE1/exercicios de fixação/temperatura.js
+++ PBE1/exercicios de fixação/temperatura.js
@@ -1,2 +1,21 @@
+function checarTemperatura(valor) {
+    if (valor > 40) {
+        return "ALERTA: Caldeira Superaquecida";
+    } else {
+        return "ALERTA: Ar muito seco";
+    }
+}
 
+function checarUmidade(valor) {
+    if (valor < 20) {
+        return "ALERTA: Ar muito seco.";
+    } else {
+        return "Umidade normal";
+    }
+}
+
+module.exports = {
+    checarUmidade,
+    checarTemperatura
+} 
 
